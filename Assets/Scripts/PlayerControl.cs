74cf5a1a39b7956b1bccc7bde07eca3d5667c8d3
devteam21 <hackmedev@gmail.com>
2021-09-07 17:06:54 +0700

    Agate - Game Pong

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+
+    public float speed = 10.0f;
+
+    // Batas atas dan bawah game scene (Batas bawah menggunakan minus (-))
+    public float yBoundary = 9.0f;
+
+    private Rigidbody2D rigidBody2D;
+    private int score;
+
+    // Titik tumbukan terakhir dengan bola, untuk menampilkan variabel-variabel fisika terkait tumbukan tersebut
+    private ContactPoint2D lastContactPoint;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Dapatkan kecepatan raket sekarang.
+        Vector2 velocity = rigidBody2D.velocity;
+
+        // Controller
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+
+        // Dapatkan posisi raket sekarang.
+        Vector3 position = transform.position;
+
+        // Jika posisi raket melewati batas atas (yBoundary), kembalikan ke batas atas tersebut.
+        if (position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }
+
+        // Jika posisi raket melewati batas bawah (-yBoundary), kembalikan ke batas atas tersebut.
+        else if (position.y < -yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+
+        // Masukkan kembali posisinya ke transform.
+        transform.position = position;
+    }
+
+    // Menaikkan skor sebanyak 1 poin
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    // Mengembalikan skor menjadi 0
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    // Mendapatkan nilai skor
+    public int Score
+    {
+        get { return score; }
+    }
+    
+    // Untuk mengakses informasi titik kontak dari kelas lain
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    // Ketika terjadi tumbukan dengan bola, rekam titik kontaknya.
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+}
---
+++ Assets/Scripts/SideWall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    [SerializeField]
+    private GameManagerPong gameManager;
+
+    // Pemain yang akan bertambah skornya jika bola menyentuh dinding ini.
+    public PlayerControl player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Akan dipanggil ketika objek lain ber-collider (bola) bersentuhan dengan dinding.
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        // Jika objek tersebut bernama "Ball":
+        if (anotherCollider.name == "Ball")
+        {
+            // Tambahkan skor ke pemain
+            player.IncrementScore();
+
+            // Jika skor pemain belum mencapai skor maksimal...
+            if (player.Score < gameManager.maxScore)
+            {
+                // ...restart game setelah bola mengenai dinding.
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+}
